de8596314a5eb746c69ec19a5cb117aef616d0e5
Your Name <you@example.com>
2026-03-11 15:23:42 +0100

    Update base URL for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/sgrullame/',
+    base: '/comforting-cannoli-405828/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
